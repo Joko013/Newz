d186c818ca2f02bf73ade4161cacf39045e2ff00
joko013 <30841710+Joko013@users.noreply.github.com>
2018-08-12 12:17:48 +0200

    V 0.2
    get distance

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
+from helpers.distance import Calculator
+
+from settings import API_KEY
+
 
 class Newz(object):
     def __init__(self):
         pass
 
     def run(self):
-        print('There is nothing here yet.')
+        origins = 'Kubanska 2 Brno'
+        destinations = 'Palachovo namesti 4 Brno'
+
+        print('There is something here, yay.')
+        calc = Calculator(api_key=API_KEY)
+        result = calc.get_distance(origins=origins, destinations=destinations)
+        print(result)
 
--- helpers/distance.py
+++ helpers/distance.py
@@ -2,5 +2,19 @@ from datetime import datetime
 
 import googlemaps
 
-from ..settings import API_KEY
 
+class Calculator(object):
+    def __init__(self, api_key):
+        self.client = googlemaps.Client(api_key)
+        self.departure_time = datetime.now()
+
+    def get_distance(self, origins, destinations,
+                     mode='transit'):
+
+        departure_time = self.departure_time
+        distance_matrix = self.client.distance_matrix(origins=origins, destinations=destinations,
+                                             mode=mode, departure_time=departure_time)
+
+        result_sec = distance_matrix['rows'][0]['elements'][0]['duration']['value']
+        result_min = round(result_sec/60)
+        return result_min
